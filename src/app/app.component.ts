59d368f32b75dcd9b2b2f3476c5a075d35c9cc63
kiwi <yachun@edetw.com>
2025-06-23 17:56:29 +0800

    母站加上警語

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule, CommonModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'my-shortcut';
+export class AppComponent implements OnInit {
+  isInternalIp = false;
+  constructor(private http: HttpClient) { }
+  
+  ngOnInit() {
+    this.http.get<any>('https://api.ipify.org?format=json').subscribe(res => {
+      this.isInternalIp = res.ip.startsWith('172.16.');
+    });
+  }
 }
